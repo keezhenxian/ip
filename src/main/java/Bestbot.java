720f10a91360c6d3bf10287d74631c293a4d2602
keezhenxian <zhenxian707@gmail.com>
2025-08-30 02:22:54 +0800

    Complete Level-3: mark/unmark and Task class

--- src/main/java/Bestbot.java
+++ src/main/java/Bestbot.java
@@ -5,20 +5,54 @@ public class Bestbot {
         System.out.println("Hello! I'm Bestbot");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
+
         while (true) {
             String line = sc.nextLine();
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.printf("%d. %s%n", i + 1, tasks.get(i));
+                    System.out.printf("%d.%s%n", i + 1, tasks.get(i));
                 }
+            } else if (line.startsWith("mark")) {
+                int idx = Integer.parseInt(line.substring(5))- 1;
+                tasks.get(idx).mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks.get(idx));
+            } else if (line.startsWith("unmark")) {
+                int idx = Integer.parseInt(line.substring(7))- 1;
+                tasks.get(idx).unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks.get(idx));
             } else {
-                tasks.add(line);
+                tasks.add(new Task(line));
                 System.out.println("added: " + line);
             }
         }
+    }
+}
+
+class Task {
+    private boolean done;
+    private final String description;
+
+    public Task(String d) {
+        this.description = d;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", done ? "X" : " ", description);
     }
 }
