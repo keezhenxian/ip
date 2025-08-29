61612419d1d9e197d77f9a1adf1d454a863ec78a
keezhenxian <zhenxian707@gmail.com>
2025-08-30 00:40:45 +0800

    Complete Level-2: add/list

--- src/main/java/Bestbot.java
+++ src/main/java/Bestbot.java
@@ -1,10 +1,24 @@
+import java.util.*;
+
 public class Bestbot {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Bestbot");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
+        while (true) {
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else if (line.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d. %s%n", i + 1, tasks.get(i));
+                }
+            } else {
+                tasks.add(line);
+                System.out.println("added: " + line);
+            }
+        }
     }
 }
